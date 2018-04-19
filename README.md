a1a6872921302694f69141ed595a66776f681dca
Mercedes <mechamena@gmail.com>
2018-04-19 19:50:24 -0300

    carousel con bootstrap

--- README.md
+++ README.md
@@ -4,5 +4,3 @@ TP 1 - Grupal
 ### Integrantes:
 + Mercedes Mena
 + Erica Garcete
-
-### Slider adaptado de: http://kenwheeler.github.io/slick/
--- js/slider.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('#slider-container').slick({
-    adaptiveHeight:true,
-    centerMode:true,
-    centerPadding:"5px"
-  });
-});
